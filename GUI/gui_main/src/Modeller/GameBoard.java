58e15630f59bc496af19fd7c8c6473dd489b5234
Nobert1 <43668248+Nobert1@users.noreply.github.com>
2018-11-16 11:30:25 +0100

    OG commit 2
    OG commit 2

---
+++ GUI/gui_main/src/Modeller/GameBoard.java
@@ -0,0 +1,43 @@
+/*
+package src.MatadorGame;
+
+public class GameBoard {
+
+    private  Squares[] board;
+    private Dice die1;
+
+    public GameBoard() {
+
+        board = new Squares[24];
+
+                board[0] = new Start(0);
+                board[1] = new Properties(1, "Gatekjøkkenet \n Burgerbaren", 1,  3, "Brown", false);
+                board[2] = new Properties(2, "Pizzahuset \n Pizzeriaet", 1, 3, "Brown", false);
+                board[3] = new Chance_Square( 3, "Sjanse \n chance");
+                board[4] = new Properties(4, "Godtebutikken \n slikbutikken", 1, 3, "Lightblue", false);
+                board[5] = new Properties(5, " Iskiosken \n Iskiosken", 1, 3, "Lightblue", false);
+                board[6] = new Jail_visiting(6, "I Fengsel \n I fængsel");
+                board[7] = new Properties(7, "Museet \n Museet", 1, 3, "pink", false);
+                board[8] = new
+                board[9] = new
+                board[10] = new
+                board[11] = new
+                board[12] = new
+                board[13] = new
+                board[14] = new
+                board[15] = new
+                board[16] = new
+                board[17] = new
+                board[18] = new
+                board[19] = new
+                board[20] = new
+                board[21] = new
+                board[22] = new
+                board[23] = new
+
+
+    }
+}
+*/
+
+
---
+++ GUI/gui_main/src/Modeller/Squares.java
@@ -0,0 +1,21 @@
+package src.MatadorGame;
+
+public class Squares {
+
+    int position;
+    String name;
+
+    public Squares(int position, String name) {
+
+        this.position = position;
+        this.name = name;
+
+    }
+
+    public String toString() {
+
+        return super.toString();
+
+    }
+}
+
